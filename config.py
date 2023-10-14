561bb00102b3e11210b06ec3664e1407b63b2925
zzzyss <117996025+zzzyss@users.noreply.github.com>
2023-10-15 00:36:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,14 +18,14 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
+PUSH_TOKEN = 9661a4f6dfbc4aa2a93d8a59ba8fe636
 
 
 '''
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
-AMAP_KEY = os.environ.get("GAODE_KEY")
+AMAP_KEY = 392ca8cad92995f3733bd957427b4217
 
 
 '''
